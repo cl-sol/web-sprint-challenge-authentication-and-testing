cf5edd2983979e24777a60233460c79c4c1b0c06
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-09-11 19:34:55 -0400

    created users router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const Users = require("../users/users-model");
+
+
 router.post('/register', (req, res) => {
   // implement registration
+  const credentials = req.body;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an error adding user to the database", err
+      })
+    })
 });
 
 router.post('/login', (req, res) => {
